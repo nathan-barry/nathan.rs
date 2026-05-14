1eaf4705f5d37624fe6cd6ca86e83c08165ab3bd
Nathan Barry <nathanbarry474@gmail.com>
2026-05-14 23:05:02 +0800

    hide menu after scrolling down

---
+++ assets/menu-fade.js
@@ -0,0 +1,31 @@
+(function () {
+  "use strict";
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", init);
+  } else {
+    init();
+  }
+
+  function init() {
+    const menu = document.querySelector(".book-menu");
+    if (!menu) return;
+
+    let ticking = false;
+    function update() {
+      menu.classList.toggle("book-menu-faded", window.scrollY > window.innerHeight);
+      ticking = false;
+    }
+
+    function onScroll() {
+      if (!ticking) {
+        window.requestAnimationFrame(update);
+        ticking = true;
+      }
+    }
+
+    window.addEventListener("scroll", onScroll, { passive: true });
+    window.addEventListener("resize", onScroll, { passive: true });
+    update();
+  }
+})();
